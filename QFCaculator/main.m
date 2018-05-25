b6e225493e11f437ec2a986411dda702d1f2a2c5
qingfeng <qingfengiosdev@gmail.com>
2018-05-25 14:20:03 +0800

    Initial Commit

---
+++ QFCaculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QFCaculator
+//
+//  Created by 李一平 on 2018/5/25.
+//  Copyright © 2018年 slwy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
